265734c5f06458b70b2e5257404c38e817af4824
Ameera Eshtewi <soulimanmd@gmail.com>
2024-02-17 13:42:15 -0800

    Initial Commit

---
+++ treehacks24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  treehacks24
+//
+//  Created by Ameera Eshtewi on 2/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
