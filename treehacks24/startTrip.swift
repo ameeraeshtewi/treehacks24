d3213ddecc9fa5c7f9ab85c585383c5edeb840f8
elaine-zhai <ehz2105@columbia.edu>
2024-02-18 03:05:52 -0800

    postTrip routes to activities

--- treehacks24/startTrip.swift
+++ treehacks24/startTrip.swift
@@ -16,6 +16,9 @@ struct startTripView: View {
     @State private var items = [String]()
     @State private var addedItems = [String]()
     
+    @State private var navigateToActivities = false
+
+    
 
     private func searchItems(searchText: String) -> [String] {
             if searchText.isEmpty { return items }
@@ -23,82 +26,90 @@ struct startTripView: View {
         }
     
     var body: some View {
-        ZStack {
-            Color(red: 76 / 255.0, green: 86 / 255.0, blue: 57 / 255.0)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Text("Start Trip")
-                    .font(.system(size: 30))
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .padding(.top, 20)
-
-                Text("Time")
-                    .padding(.top, 30)
-                    .padding(.bottom, 10)
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                    .font(.system(size: 20))
-                
-                TextField("Set Time", text: $startTimeText)
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color(red: 150 / 255.0, green: 165 / 255.0, blue: 120 / 255.0))
-                    .cornerRadius(5.0)
-                    .multilineTextAlignment(.center)
-                    .frame(width: 200) // Specify the width here
-                
-                .padding()
-                
-                TextField("Search...", text: $searchText)
-                    .padding(7)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .padding(.horizontal, 10)
-                    .onSubmit {
-                        addSearchTextToList()
-                    }
-                    .submitLabel(.done)
+        NavigationView{
+            ZStack {
+    
+                Color(red: 76 / 255.0, green: 86 / 255.0, blue: 57 / 255.0)
                     .edgesIgnoringSafeArea(.all)
-                List {
-                    ForEach(addedItems, id: \.self) { addedItem in
-                        HStack {
-                            Text(addedItem)
-                            Spacer()
-                            Button(action: {
-                                if let index = addedItems.firstIndex(of: addedItem) {
-                                    addedItems.remove(at: index)
-                                }
-                            }) {
-                                Image(systemName: "minus.circle.fill")
-                                    .foregroundColor(.red)
-                            }
-                            
-                        }
-                    }
-                    .onDelete(perform: removeItems)
-                }.background(Color(red: 150 / 255.0, green: 165 / 255.0, blue: 120 / 255.0))
-                    .padding()
-                .scrollContentBackground(.hidden)
-
-                Spacer()
-                                
-                Button(action: {
-                    print("Start Trip Button Tapped")
-                }) {
-                    Text("Post Trip")
-                        .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
-                        .background(Color(red: 150 / 255.0, green: 165 / 255.0, blue: 120 / 255.0))
-                        .cornerRadius(8)
-                        .font(.system(size: 14))
+                VStack {
+                    Text("Start Trip")
+                        .font(.system(size: 30))
+                        .fontWeight(.bold)
                         .foregroundColor(.white)
+                        .padding(.top, 20)
+
+                    Text("Time")
+                        .padding(.top, 30)
+                        .padding(.bottom, 10)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .font(.system(size: 20))
+                    
+                    TextField("Set Time", text: $startTimeText)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color(red: 150 / 255.0, green: 165 / 255.0, blue: 120 / 255.0))
+                        .cornerRadius(5.0)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 200) // Specify the width here
+                    
+                    .padding()
+                    
+                    TextField("Search...", text: $searchText)
+                        .padding(7)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal, 10)
+                        .onSubmit {
+                            addSearchTextToList()
+                        }
+                        .submitLabel(.done)
+                        .edgesIgnoringSafeArea(.all)
+                    List {
+                        ForEach(addedItems, id: \.self) { addedItem in
+                            HStack {
+                                Text(addedItem)
+                                Spacer()
+                                Button(action: {
+                                    if let index = addedItems.firstIndex(of: addedItem) {
+                                        addedItems.remove(at: index)
+                                    }
+                                }) {
+                                    Image(systemName: "minus.circle.fill")
+                                        .foregroundColor(.red)
+                                }
+                                
+                            }
+                        }
+                        .onDelete(perform: removeItems)
+                    }.background(Color(red: 150 / 255.0, green: 165 / 255.0, blue: 120 / 255.0))
+                        .padding()
+                    .scrollContentBackground(.hidden)
+
+                    Spacer()
+                                    
+                    Button(action: {
+                        navigateToActivities = true
+                        print("Start Trip Button Tapped")
+                    }) {
+                        Text("Post Trip")
+                            .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                            .background(Color(red: 150 / 255.0, green: 165 / 255.0, blue: 120 / 255.0))
+                            .cornerRadius(8)
+                            .font(.system(size: 14))
+                            .foregroundColor(.white)
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 20)
+                    NavigationLink(destination: activity(), isActive: $navigateToActivities) { EmptyView() }
+                    .hidden()
+                    Spacer()
+                   
                 }
-                .padding(.horizontal)
-                .padding(.top, 20)
-                Spacer()
-               
+                
             }
+            
         }
     }
     private func addSearchTextToList() {
