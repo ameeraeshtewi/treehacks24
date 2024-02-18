a6e75a80983c5ed19058626681fedb765c61c7b3
Alicia <42559357+aliciabao@users.noreply.github.com>
2024-02-17 23:40:40 -0800

    fixed the colors for the tabview icons

---
+++ treehacks24/browseTrips.swift
@@ -0,0 +1,18 @@
+//
+//  browseTrips.swift
+//  treehacks24
+//
+//  Created by Alicia Bao on 2/17/24.
+//
+
+import SwiftUI
+
+struct browseTrips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    browseTrips()
+}
