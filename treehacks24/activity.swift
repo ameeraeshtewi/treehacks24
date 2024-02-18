c32367d46c3b3db4522eb787c66898dba5271e9b
Ameera Eshtewi <soulimanmd@gmail.com>
2024-02-17 21:08:54 -0800

    framework for activity page

---
+++ treehacks24/activity.swift
@@ -0,0 +1,18 @@
+//
+//  activity.swift
+//  treehacks24
+//
+//  Created by Ameera Eshtewi on 2/17/24.
+//
+
+import SwiftUI
+
+struct activity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    activity()
+}
