bd46ba5ca27b448d50bea77e42cd166ab15b2395
Alicia <42559357+aliciabao@users.noreply.github.com>
2024-02-18 00:46:17 -0800

    browse trips pull up details

--- treehacks24/browse.swift
+++ treehacks24/browse.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct browse: View {
+    @State private var detailedView = false
     var body: some View {
         ZStack{
             Color(red: 0.2980392156862745, green: 0.33725490196078434, blue: 0.2235294117647059).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -29,10 +30,50 @@ struct browse: View {
                             .frame(alignment: .leading)
                     }
                     HStack{
-                        Text("See full trip details")
+                        Button(action:{
+                            detailedView = true
+                        }){
+                            Text("See full trip details")
+                        }
                             .padding(10)
                             .background(Color(red: 0.2980392156862745, green: 0.33725490196078434, blue: 0.2235294117647059))
                             .foregroundColor(Color(red: 1, green: 0.9725490196078431, blue: 0.9098039215686274))
+                            .popover(isPresented:$detailedView){
+                                ZStack{
+                                    Color(red: 1, green: 0.9725490196078431, blue: 0.9098039215686274)
+                                    VStack{
+                                        HStack{
+                                            Button(action:{
+                                                detailedView = false
+                                            }){
+//                                               Image("exit")
+                                            }
+                                            Spacer()
+                                            
+                                        }
+                                        Text("Alicia’s Trip to Trader Joes")
+                                            .font(.largeTitle
+                                                .weight(.bold))
+                                        Text("Your Items")
+                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                            .padding(.top, 10)
+                                        Text("Bananas")
+                                        Text("Oranges")
+                                        Text("Belvita crackers")
+                                        Text("Pickup Location")
+                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                            .padding(.top, 10)
+                                        Text("McIver Residence Hall 103 Raleigh St. Chapel Hill, NC 27514")
+                                            .frame(maxWidth: 250)
+                                        Text("Pickup Time")
+                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                            .padding(.top, 10)
+                                        Text("Today at 5:00 pm")
+                                        Spacer()
+                                    }.padding()
+                                        .foregroundStyle(Color(red: 0.2980392156862745, green: 0.33725490196078434, blue: 0.2235294117647059))
+                                }
+                            }
                         Spacer()
                         Button(action: {}){
                         Text("Join Cart")
