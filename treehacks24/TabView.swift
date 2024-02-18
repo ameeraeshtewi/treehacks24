c51979ab0e905fe4c1d976123e71c697a576d34a
Alicia <42559357+aliciabao@users.noreply.github.com>
2024-02-17 23:41:34 -0800

    try agained

--- treehacks24/TabView.swift
+++ treehacks24/TabView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct TabViewExample: View {
     init() {
-    UITabBar.appearance().backgroundColor = UIColor(Color(red: 0.2980392156862745, green: 0.33725490196078434, blue: 0.2235294117647059)
-                                        
-)
+    UITabBar.appearance().backgroundColor = UIColor(Color(red: 0.2980392156862745, green: 0.33725490196078434, blue: 0.2235294117647059))
     }
     var body: some View {
         TabView() {
@@ -19,26 +17,25 @@ struct TabViewExample: View {
                 .tabItem {
                     Text("Your List")
                     Image("list")
-                    
                 }
-//            browseTrips()
-//                .tabItem {
-//                    Text("")
-//                    Image("map")
-//                }
-//            activity()
-//                .tabItem {
-//                    Text("Activity")
-//                    Image(systemName: "receipt")
-//                }
-//            profile()
-//                .tabItem {
-//                    Text("Profile")
-//                    Image(systemName: "profile")
-//                }
+            browseTrips()
+                .tabItem {
+                    Text("Browse Trips")
+                    Image("map")
+                }
+            activity()
+                .tabItem {
+                    Text("Activity")
+                    Image("receipt")
+                }
+            AccountView()
+                .tabItem {
+                    Text("Profile")
+                    Image("profile")
+                }
+            
         }
         .accentColor(Color(red: 1, green: 0.9725490196078431, blue: 0.9098039215686274))
-
     }
     
 }
